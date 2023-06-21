16687cbb74c49a9237e0439b55ecc02889c765c0
Rajesh Kumar Gupta <rajeshkumar.gupta@basware.com>
2023-06-21 10:49:09 +0530

    Added main sol

--- AWSLambdaFirstProj/Function.cs
+++ AWSLambdaFirstProj/Function.cs
@@ -1,3 +1,5 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
 using Amazon.Lambda.Core;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -7,15 +9,25 @@ namespace AWSLambdaFirstProj;
 
 public class Function
 {
-    
+
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
     /// <param name="input"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public async Task<Employee> FunctionHandler(string input, ILambdaContext context)
     {
-        return input.ToUpper();
+        var dynamoDBcontext = new DynamoDBContext(new AmazonDynamoDBClient());
+        var empObj = await dynamoDBcontext.LoadAsync<Employee>(input);
+        return empObj;
+    }
+
+    public class Employee
+    {
+        [DynamoDBHashKey]
+        public string Id { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
     }
 }
